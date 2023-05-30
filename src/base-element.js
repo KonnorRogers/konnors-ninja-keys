f2d8a5c0164f2089ccc15c6ca516faba244922bb
Konnor Rogers <konnor5456@gmail.com>
2023-05-30 17:49:57 -0400

    working on it...

--- src/base-element.js
+++ src/base-element.js
@@ -1,6 +1,6 @@
 // @ts-check
 import { LitElement } from "lit";
-import { DefineableMixin } from "web-component-define";
+import { DefineableMixin } from "web-component-define/exports/define.js";
 import { ScopedElementsMixin } from "@open-wc/scoped-elements"
 
 export class BaseElement extends ScopedElementsMixin(DefineableMixin(LitElement)) {}
