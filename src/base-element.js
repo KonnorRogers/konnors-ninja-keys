32778e9b449b679bc3a0423e7d0d1c97cb476629
Konnor Rogers <konnor5456@gmail.com>
2023-06-02 12:39:05 -0400

    dont constrain the mixin

--- src/base-element.js
+++ src/base-element.js
@@ -1,8 +1,8 @@
 // @ts-check
-import { LitElement } from "lit";
+import {
+  LitElement,
+} from "lit";
 import { DefineableMixin } from "web-component-define";
 import { LitScopedElementsMixin } from "web-component-define/exports/lit.js"
 
-export class BaseElement extends DefineableMixin(LitElement) {}
-
-const base = new BaseElement()
+export class BaseElement extends DefineableMixin(LitScopedElementsMixin(LitElement)) {}
