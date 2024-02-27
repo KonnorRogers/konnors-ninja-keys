8a4632bc84b4a7fb8f982084078e36a229443f83
konnorrogers <konnor5456@gmail.com>
2024-02-27 16:38:05 -0500

    cleaning up documentation

--- docs/plugins/builders/changelog_generator.rb
+++ docs/plugins/builders/changelog_generator.rb
@@ -2,21 +2,34 @@ class Builders::ChangelogGenerator < SiteBuilder
   def build
     hook :site, :post_read do
       generate_changelog
+      generate_readme
     end
 
     hook :site, :post_reload do
       generate_changelog
+      generate_readme
     end
   end
 
   def generate_changelog
     changelog_file = File.expand_path("../../../CHANGELOG.md", __dir__)
-    add_resource :documentation, "references/00-changelog.md" do
+    add_resource :documentation, "references/01-changelog.md" do
       title "Changelog"
       permalink "/references/changelog/"
       category "references"
       layout "doc"
       content File.read(changelog_file)
+    end
+  end
+
+  def generate_readme
+    readme_file = File.expand_path("../../../README.md", __dir__)
+    add_resource :documentation, "references/00-readme.md" do
+      title "README"
+      permalink "/references/readme/"
+      category "references"
+      layout "doc"
+      content File.read(readme_file)
     end
   end
 end
--- docs/src/_documentation/references/index.md
+++ docs/src/_documentation/references/index.md
@@ -1,4 +1,5 @@
 ---
 title: References
 layout: index
+permalink: /references/
 ---
